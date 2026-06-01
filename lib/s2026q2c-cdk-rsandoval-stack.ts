9c3bf3ae72a9b2c8c1b582ee221aa7bc8979c141
Ricardo Sandoval <ricardo.sandoval@blend360.com>
2026-06-01 14:47:33 -0500

    Initial commit

---
+++ lib/s2026q2c-cdk-rsandoval-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class S2026Q2CCdkRsandovalStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'S2026Q2CCdkRsandovalQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/s2026q2c-cdk-rsandoval.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S2026Q2CCdkRsandoval from '../lib/s2026q2c-cdk-rsandoval-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s2026q2c-cdk-rsandoval-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S2026Q2CCdkRsandoval.S2026Q2CCdkRsandovalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
